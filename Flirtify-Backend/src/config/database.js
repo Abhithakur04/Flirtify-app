625f3f7da42bf69f64e72384c0b2913be5e926bc
Abhithakur04 <152187846+Abhithakur04@users.noreply.github.com>
2025-08-31 15:39:30 +0530

    Update database.js

--- Flirtify-Backend/src/config/database.js
+++ Flirtify-Backend/src/config/database.js
@@ -1,7 +1,7 @@
   const mongoose=require("mongoose");
   const connectDB=async()=>{
     //mongoose.connect will return a promise
-    await mongoose.connect("mongodb://localhost:27017/devTinder");
+    await mongoose.connect(process.env.MONGO_URI);
   }
   
   module.exports=connectDB;
