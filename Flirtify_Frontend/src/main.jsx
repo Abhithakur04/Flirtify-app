a91fe60d6a0ec6d3847395c4ec3cf10c388d059b
Abhithakur04 <152187846+Abhithakur04@users.noreply.github.com>
2025-09-02 15:57:20 +0530

    Update main.jsx

--- Flirtify_Frontend/src/main.jsx
+++ Flirtify_Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"
 
+document.documentElement.setAttribute("data-theme", "light");
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
